ab1b727012db4dce682cd09de39cdbfe1b207002
Rafid-tawhid <rafidtawhid@gmail.com>
2025-08-25 18:08:25 +0600

    make all grid item same

--- lib/screens/Products/product_home_screen.dart
+++ lib/screens/Products/product_home_screen.dart
@@ -12,6 +12,7 @@ import 'package:yunusco_group/screens/Products/widgets/production_screen.dart';
 import 'package:yunusco_group/utils/colors.dart';
 import 'package:yunusco_group/utils/constants.dart';
 
+import '../../common_widgets/dashboard_item_card.dart';
 import '../../utils/colors.dart';
 import 'garments_requisation_screen.dart';
 import 'get_all_style_screen.dart';
@@ -20,107 +21,112 @@ import 'widgets/buyers_screen.dart';
 class ProductHomeScreen extends StatelessWidget {
   ProductHomeScreen({super.key});
 
-  final List<String> _list=['Production\nSummary','Production\nEfficiency','Production\nMonthly/Yearly','Buyers','Stylewise\nEfficiency'];
 
   @override
   Widget build(BuildContext context) {
+    final List<DashboardMenuItem> menuItems = [
+      DashboardMenuItem(
+        name: 'Production\nSummary',
+        icon: Icons.assessment,
+        cardColor: const Color(0xFFE3F2FD), // Light blue
+        iconColor: const Color(0xFF1976D2), // Dark blue
+        onTap: () async {
+          var pp = context.read<ProductProvider>();
+          await pp.getAllProductionDashboard();
+          if (pp.productionDashboardModel != null) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ProductionDashboard()),
+            );
+          }
+        },
+      ),
+      DashboardMenuItem(
+        name: 'Production\nEfficiency',
+        icon: Icons.assured_workload,
+        cardColor: const Color(0xFFE8F5E9), // Light green
+        iconColor: const Color(0xFF388E3C), // Dark green
+        onTap: () async{
+          var pp = context.read<ProductProvider>();
+          var currentDate = convertDate(DateTime.now());
+          await pp.getProductionEfficiencyReport(currentDate);
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ProductionEfficiencyScreen()),
+          );
+        },
+      ),
+      DashboardMenuItem(
+        name: 'Production\nMonthly/Yearly',
+        icon: Icons.calendar_month,
+        cardColor: const Color(0xFFFFEBEE), // Light red
+        iconColor: const Color(0xFFD32F2F), // Dark red
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const ProductionSummaryScreen()),
+          );
+        },
+      ),
+      DashboardMenuItem(
+        name: 'Buyers',
+        icon: Icons.people_alt_outlined,
+        cardColor: const Color(0xFFF3E5F5), // Light purple
+        iconColor: const Color(0xFF7B1FA2), // Dark purple
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => BuyersScreen()),
+          );
+        },
+      ),
+      DashboardMenuItem(
+        name: 'Stylewise\nEfficiency',
+        icon: Icons.style_sharp,
+        cardColor: const Color(0xFFFFF8E1), // Light amber
+        iconColor: const Color(0xFFFFA000), // Dark amber
+        onTap: () async{
+          var pp = context.read<ProductProvider>();
+          await pp.getAllStyleData();
+          if (pp.buyerStyleList.isNotEmpty) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => StyleSelectionScreen()),
+            );
+          }
+        },
+      ),
+    ];
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.blue.shade50,
       appBar: AppBar(
-        title:  Text('Production Menu',style: customTextStyle(18, Colors.white, FontWeight.w600),),
+        title: Text(
+          'Production Menu',
+          style: customTextStyle(18, Colors.white, FontWeight.w600),
+        ),
         centerTitle: true,
         backgroundColor: myColors.primaryColor,
-        iconTheme: IconThemeData(
-          color: Colors.white
-        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        systemOverlayStyle: SystemUiOverlayStyle.light,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(12.0),
+        padding: const EdgeInsets.all(16.0),
         child: GridView.builder(
-          itemCount: _list.length,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            mainAxisSpacing: 16,
-            crossAxisSpacing: 16,
-            childAspectRatio: 0.8,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 0.85,
           ),
-          padding: const EdgeInsets.all(16),
+          itemCount: menuItems.length,
           itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () async {
-                HapticFeedback.lightImpact();
-
-                if (index == 0) {
-                  var pp=context.read<ProductProvider>();
-                  await pp.getAllProductionDashboard();
-                  if(pp.productionDashboardModel!=null){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) =>  ProductionDashboard()),
-                    );
-                  }
-                }
-                if (index == 1) {
-
-                  var pp=context.read<ProductProvider>();
-                  var currentDate=convertDate(DateTime.now());
-
-                  await pp.getProductionEfficiencyReport(currentDate);
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) =>  ProductionEfficiencyScreen()),
-                    );
-
-                }
-                if (index == 2) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ProductionSummaryScreen()),
-                  );
-                }
-                else if (index == 3) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) =>  BuyersScreen()),
-                  );
-                }
-                else if (index == 4) {
-                  var pp=context.read<ProductProvider>();
-
-                  await pp.getAllStyleData();
-
-                  if(pp.buyerStyleList.isNotEmpty){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) =>  StyleSelectionScreen()),
-                    );
-                  }
-
-
-                }
-
-
-              },
-              child: Card(
-                color: Colors.white,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                          getIcon(index),
-                          size: 44,
-                          color: myColors.primaryColor
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        _list[index],
-                        style:customTextStyle(16, myColors.primaryColor, FontWeight.w600),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
+            return ReusableDashboardCard(
+              menu: DashboardMenuItem(
+                name: menuItems[index].name,
+                icon: menuItems[index].icon,
+                cardColor: menuItems[index].cardColor,
+                iconColor: menuItems[index].iconColor,
+                onTap: () => menuItems[index].onTap(),
               ),
             );
           },
@@ -129,40 +135,13 @@ class ProductHomeScreen extends StatelessWidget {
     );
   }
 
-  IconData? getIcon(int index) {
-    if(index==0){
-      return Icons.assessment;
-    }
-    if(index==1)
-      {
-        return Icons.assured_workload;
-      }
-    if(index==2)
-    {
-      return Icons.calendar_month;
-    }
-    if(index==3)
-    {
-      return Icons.people_alt_outlined;
-    }
-    if(index==4)
-    {
-      return Icons.style_sharp;
-    }
-    if(index==5)
-    {
-      return Icons.security_update_good;
-    }
-  }
+  String convertDate(DateTime date) {
+    // Get the individual components
+    String year = date.year.toString();
+    String month = date.month.toString().padLeft(2, '0');
+    String day = date.day.toString().padLeft(2, '0');
 
-  convertDate(DateTime date) {
-      // Get the individual components
-      String year = date.year.toString();
-      String month = date.month.toString().padLeft(2, '0'); // Ensures two digits
-      String day = date.day.toString().padLeft(2, '0'); // Ensures two digits
-
-      // Combine them in YYYY-MM-DD format
-      return '$year-$month-$day';
-
+    // Combine them in YYYY-MM-DD format
+    return '$year-$month-$day';
   }
 }
