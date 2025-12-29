59862376074a90ffee2af3fcbbc13aa9a4cfaf9c
Rafid-tawhid <rafidtawhid.com>
2025-12-29 15:58:29 +0600

    machine problem report

--- lib/main.dart
+++ lib/main.dart
@@ -86,7 +86,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-//
+
 
 Future<void> setupNotificationChannel() async {
   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
